1d34cad903368fdff8d2b6ba6cfaf40f9428b29c
Triangle717 <le717@users.noreply.github.com>
2013-10-18 19:23:22 -0400

    Minor

--- CRTC.js
+++ CRTC.js
@@ -1,23 +1,23 @@
 /*
-    Cycles Render Time Calculator
+    Cycles Render Time Calculator (CRTC)
     Calculate how long your Blender Cycles Engine renders will take.
 
     Created 2013 Triangle717 & rioforce
     <http://Triangle717.WordPress.com/>
     <http://rioforce.wordpress.com/>
 
-    Cycles Render Time Calculator is free software: you can redistribute it and/or modify
+    CRTC is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
     the Free Software Foundation, either version 3 of the License, or
     (at your option) any later version.
 
-    Cycles Render Time Calculator is distributed in the hope that it will be useful,
+    CRTC is distributed in the hope that it will be useful,
     but WITHOUT ANY WARRANTY; without even the implied warranty of
     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
     GNU General Public License for more details.
 
     You should have received a copy of the GNU General Public License
-    along with Cycles Render Time Calculator. If not, see <http://www.gnu.org/licenses/>.
+    along with CRTC If not, see <http://www.gnu.org/licenses/>.
 
 -------------------------------------
 Cycles Render Time Calculator - Web Version
@@ -31,8 +31,9 @@ var creator = "Triangle717 and rioforce"
 // CRTC icon
 var app_icon = "Icon.ico"
 
-// Generic Animation Render Time
 var videoRender = function() {
+    /* Generic Animation Render Time */
+
     var videoframes = prompt("\nHow many frames are in your animation? ");
     var videoframetime = prompt("How long does a single frame take to render (in seconds)? ");
 
